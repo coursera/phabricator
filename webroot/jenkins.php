e53f55720b7374a0d8bff7444e7064d6364a523b
Bryan Kane <bryan@coursera.org>
2015-08-05 14:58:52 -0700

    Add jenkins proxy, health check file, and robots

---
+++ webroot/jenkins.php
@@ -0,0 +1,14 @@
+<?php
+  $query = $_SERVER['QUERY_STRING'];
+  #print "HEY $query<br>";
+  $script = $_SERVER['SCRIPT_NAME'];
+  $script = preg_replace('#/?jenkins.php/?#', '', $script);
+
+  # http://192.168.56.101:81/jenkins.php/jenkins.dkandu.me/job/c3-diph/buildWithParameters?DIFF_ID=926
+  $username = "phabricator";
+  $password = "fbd4590d68d267ccc598931c9e329e92";
+
+  $url2 = "$script?$query";
+  print "Kicking off a build: $url2\n";
+  system("curl -X POST 'https://$username:$password@$url2'");
+?>
